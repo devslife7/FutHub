33281ef4d05575049db959bbe470e916ec4e3548
Marcos Velasco <devslife7@gmail.com>
2020-09-24 23:34:43 -0400

    cleaned up comments

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { setCurrentUser } from './actions/user'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Container from '@material-ui/core/Container'
-import NavBar from './containers/NavBar'
-import Leagues from './containers/Leagues'
-import LogIn from './components/LogIn'
-import SignUp from './components/SignUp'
-import StickyFooter from './components/StickyFooter'
-import Friends from './components/Friends'
-import Favorites from './components/Favorites'
-import Profile from './components/Profile'
-import Standings from './components/Standings'
-import Upcoming from './components/Upcoming'
+import React, { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { setCurrentUser } from "./actions/user"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Container from "@material-ui/core/Container"
+import NavBar from "./containers/NavBar"
+import Leagues from "./containers/Leagues"
+import LogIn from "./components/LogIn"
+import SignUp from "./components/SignUp"
+import StickyFooter from "./components/StickyFooter"
+import Friends from "./components/Friends"
+import Favorites from "./components/Favorites"
+import Profile from "./components/Profile"
+import Standings from "./components/Standings"
+import Upcoming from "./components/Upcoming"
 
 const usersURL = "http://localhost:3000/users/"
 
 function App() {
-  console.log('renders App')
+  console.log("renders App")
   const dispatch = useDispatch()
 
-  useEffect( () => {
-    console.log('renders App on mount')
+  useEffect(() => {
+    console.log("renders App on mount")
 
-    if (!!localStorage.userId){
-      fetch( usersURL + localStorage.userId )
-      .then( resp => resp.json() )
-      .then( user => {
-          // console.log('this is the return of the server: ', user)
+    if (!!localStorage.userId) {
+      fetch(usersURL + localStorage.userId)
+        .then(resp => resp.json())
+        .then(user => {
           dispatch(setCurrentUser(user))
         })
     }
@@ -35,7 +34,7 @@ function App() {
 
   return (
     <Router>
-      <NavBar/>
+      <NavBar />
       <Container>
         <Route exact path='/' component={Leagues} />
         <Route exact path='/leagues' component={Leagues} />
@@ -47,7 +46,7 @@ function App() {
         <Route exact path='/standings' component={Standings} />
         <Route exact path='/upcoming' component={Upcoming} />
       </Container>
-      <StickyFooter/>
+      <StickyFooter />
     </Router>
   )
 }
