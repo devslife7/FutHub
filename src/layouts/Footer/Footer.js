618e44931c7543e7c993ed37d52feedb120243d6
Marcos Velasco <devslife7@gmail.com>
2021-03-17 23:59:04 -0400

    fixing overflow of items in paper components

--- src/layouts/Footer/Footer.js
+++ src/layouts/Footer/Footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
+import { CssBaseline } from '@material-ui/core'
 
 function Copyright() {
   return (
@@ -36,16 +37,16 @@ export default function Footer() {
 
   return (
     <div className={classes.root}>
-      {/* <CssBaseline />
-      <Container component="main" className={classes.main} maxWidth="sm">
-        <Typography variant="h2" component="h1" gutterBottom>
+      <CssBaseline />
+      {/* <Container component='main' className={classes.main} maxWidth='sm'>
+        <Typography variant='h2' component='h1' gutterBottom>
           Sticky footer
         </Typography>
-        <Typography variant="h5" component="h2" gutterBottom>
+        <Typography variant='h5' component='h2' gutterBottom>
           {'Pin a footer to the bottom of the viewport.'}
           {'The footer will move as the main element of the page grows.'}
         </Typography>
-        <Typography variant="body1">Sticky footer placeholder.</Typography>
+        <Typography variant='body1'>Sticky footer placeholder.</Typography>
       </Container> */}
       <footer className={classes.footer}>
         <Container maxWidth='sm'>
--- src/views/Profile/components/LeagueCardSmall.js
+++ src/views/Profile/components/LeagueCardSmall.js
@@ -1,36 +1,25 @@
-import React from 'react';
+import React from 'react'
 // import { useSelector, useDispatch } from 'react-redux';
 // import { addFavoriteLeague, removeFavoriteLeague } from '../actions/user';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import ButtonBase from '@material-ui/core/ButtonBase';
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+// import ButtonBase from '@material-ui/core/ButtonBase'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
+const useStyles = makeStyles(theme => ({
   paper: {
     padding: theme.spacing(1),
-    width: '250px',
-    height: '55px'
-  },
-  image: {
-    width: 40,
-    height: 40,
   },
   img: {
-    margin: 'auto',
-    display: 'block',
-    maxWidth: '100%',
-    maxHeight: '100%',
+    width: 40,
+    height: 40,
   },
   links: {
     textDecoration: 'none',
     color: 'inherit',
-  }
-}));
+  },
+}))
 
 function LeagueCardSmall({ currentLeague }) {
   const classes = useStyles()
@@ -39,31 +28,35 @@ function LeagueCardSmall({ currentLeague }) {
   // const favLeagues = useSelector(state => state.user.currentUser.favLeagues)
 
   return (
-    <div className={classes.root}>
+    <>
       <Paper className={classes.paper}>
-        <Grid container spacing={2}>
-          <Grid item>
-            <ButtonBase className={classes.image}>
-              <img className={classes.img} src={currentLeague.logo} alt='league logo' />
-            </ButtonBase>
+        <Grid container spacing={2} direction='row' alignItems='center'>
+          <Grid item style={{ backgroundColor: 'red' }}>
+            <img className={classes.img} src={currentLeague.logo} alt='league logo' />
           </Grid>
-          <Grid item xs={12} sm container>
-            <Grid item xs container direction="column" spacing={2}>
+          <Grid item xs={12} container style={{ backgroundColor: 'red' }}>
+            <Grid item xs container direction='column' spacing={2}>
               <Grid item xs>
-                <Typography gutterBottom variant="subtitle1">
+                <Typography gutterBottom variant='subtitle1'>
                   {currentLeague.name}
                 </Typography>
-                <Typography variant="body2" gutterBottom>
-                  { currentLeague.flag && <img style={{width: '23px', marginRight: '10px'}} src={currentLeague.flag} alt='league logo'/> }
-                  {currentLeague.country === "World" ? "International" : currentLeague.country}
+                <Typography variant='body2' gutterBottom>
+                  {currentLeague.flag && (
+                    <img
+                      style={{ width: '23px', marginRight: '10px' }}
+                      src={currentLeague.flag}
+                      alt='league logo'
+                    />
+                  )}
+                  {currentLeague.country === 'World' ? 'International' : currentLeague.country}
                 </Typography>
               </Grid>
             </Grid>
           </Grid>
         </Grid>
       </Paper>
-    </div>
-  );
+    </>
+  )
 }
 
 export default LeagueCardSmall
