90b4cbb23c67cd48aa36de31de54d276acedbc7d
Marcos Velasco <devslife7@gmail.com>
2021-03-25 22:08:32 -0400

    added paper to home

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,5 +1,18 @@
+import { makeStyles, Paper } from '@material-ui/core'
 import React from 'react'
 
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: '10px',
+  },
+}))
+
 export default function Home() {
-  return <div>Home</div>
+  const classes = useStyles()
+
+  return (
+    <>
+      <Paper className={classes.paper}>Welcome to FutFriends</Paper>
+    </>
+  )
 }
