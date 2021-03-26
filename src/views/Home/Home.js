d7d0ba59780905a90b79067886cb9dec526948d0
Marcos Velasco <devslife7@gmail.com>
2021-03-25 22:16:36 -0400

    added description and button to home page

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,9 +1,20 @@
-import { makeStyles, Paper } from '@material-ui/core'
+import { Button, makeStyles, Paper, Typography } from '@material-ui/core'
 import React from 'react'
 
 const useStyles = makeStyles(theme => ({
   paper: {
     marginTop: '10px',
+    padding: '20px',
+  },
+  title: {
+    fontSize: '2rem',
+    fontWeight: '300',
+    textAlign: 'center',
+  },
+  description: {
+    fontSize: '1rem',
+    textAlign: 'center',
+    color: 'gray',
   },
 }))
 
@@ -12,7 +23,16 @@ export default function Home() {
 
   return (
     <>
-      <Paper className={classes.paper}>Welcome to FutFriends</Paper>
+      <Paper className={classes.paper}>
+        <Typography className={classes.title}>Welcome to FutFriends</Typography>
+        <Typography className={classes.description}>
+          FutFriends is an app for soccer fanatics to browse leagues, upcoming games and setup watch parties
+          with your friends.
+        </Typography>
+        <Button variant='outlined' style={{ textDecoration: 'none' }}>
+          Get Started
+        </Button>
+      </Paper>
     </>
   )
 }
