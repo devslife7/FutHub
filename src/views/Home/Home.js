d4ba579e2f08da05f46c1bb6db65907be13286c5
Marcos Velasco <devslife7@gmail.com>
2021-07-26 00:45:55 -0400

    improved home screen

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,32 +1,32 @@
 import React from 'react'
 import 'date-fns'
-import DateFnsUtils from '@date-io/date-fns'
 import { Button, makeStyles, Paper, Typography } from '@material-ui/core'
-import { KeyboardDatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers'
 import { useSelector } from 'react-redux'
 import { useHistory } from 'react-router'
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(() => ({
   paper: {
     marginTop: '30px',
     padding: '50px',
   },
   title: {
-    fontSize: '3.5rem',
+    fontSize: '3.7rem',
     fontWeight: '600',
     textAlign: 'center',
-    margin: '80px 0',
+    margin: '8rem 0 3rem 0',
+    color: 'white',
   },
   description: {
-    fontSize: '1.6rem',
+    fontSize: '1.5rem',
     textAlign: 'center',
-    color: 'gray',
-    margin: '0 100px',
+    color: 'white',
+    margin: '0 auto',
+    maxWidth: '70rem',
   },
   button: {
     display: 'block',
-    margin: '140px auto',
-    fontSize: '1.3rem',
+    margin: '70px auto',
+    fontSize: '1.2rem',
   },
 }))
 
@@ -42,17 +42,17 @@ export default function Home() {
 
   return (
     <>
-      <Paper className={classes.paper}>
-        <Typography className={classes.title}>Welcome to FutFriends</Typography>
-        <Typography className={classes.description}>
-          FutFriends is an app for soccer fanatics who enjoy watching soccer games with their friends but
-          coordinating a watch party is not always that easy. Here you can browse leagues, upcoming games and
-          setup watch parties with your friends.
-        </Typography>
-        <Button variant='contained' color='primary' className={classes.button} onClick={handleGetStarted}>
-          Get Started
-        </Button>
-      </Paper>
+      {/* <Paper className={classes.paper}> */}
+      <Typography className={classes.title}>Welcome to FutFriends</Typography>
+      <Typography className={classes.description}>
+        FutFriends is an app for soccer fanatics who enjoy watching soccer games with their friends but
+        coordinating a watch party is not always that easy. Here you can browse leagues, upcoming games and
+        setup watch parties with your friends.
+      </Typography>
+      <Button variant='contained' color='primary' className={classes.button} onClick={handleGetStarted}>
+        Get Started
+      </Button>
+      {/* </Paper> */}
     </>
   )
 }
